391b0d54c9d25e63caaa867724d11ffd20bd0947
Mansi jain <43719942+Mansi24jain@users.noreply.github.com>
2019-07-21 10:35:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const replace = require('./replace.js');
 //json
-let json = fs.readFileSync('../lecture_9/data.json');
+let json = fs.readFileSync('./data.json');
 let jsonObj = JSON.parse(json);
 //templates
 const producthtml = fs.readFileSync('./templates/template-product.html').toString();
